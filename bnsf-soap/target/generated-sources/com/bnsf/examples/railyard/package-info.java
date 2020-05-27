d3dddad2d02a070bc50740560baa0e2d95fe2c23
John Gammon <jgammon612@yahoo.com>
2020-05-27 17:02:57 -0500

    first commit

---
+++ bnsf-soap/target/generated-sources/com/bnsf/examples/railyard/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://examples.bnsf.com/railyard.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.bnsf.examples.railyard;
