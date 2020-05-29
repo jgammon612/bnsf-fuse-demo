93e80fad924ea1c09d8f9f2d10c544a01db66acc
Josh Reagan <jreagan@redhat.com>
2020-05-29 11:36:50 -0600

    Cleanup/remove IDE files and build directories.

--- bnsf-soap/target/generated-sources/com/bnsf/examples/railyard/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://examples.bnsf.com/railyard.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.bnsf.examples.railyard;
